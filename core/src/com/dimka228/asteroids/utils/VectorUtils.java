27b6844efe3b7e87d028b06d5c3a5809992423ee
DimaLedentsov <63366388+DimaLedentsov@users.noreply.github.com>
2022-08-14 17:54:39 +0300

    misc

--- core/src/com/dimka228/asteroids/utils/VectorUtils.java
+++ core/src/com/dimka228/asteroids/utils/VectorUtils.java
@@ -6,4 +6,10 @@ public class VectorUtils {
     public static Vector2 fromAngle(float angle, float m){
         return new Vector2((float)Math.cos(angle)*m, (float)Math.sin(angle)*m);
     }
+    public static void limitLow(Vector2 v, float l){
+        if(v.len()<l){
+            float angle = v.angleRad();
+           // v.set(Vector2.Y).setAn
+        }
+    }
 }
